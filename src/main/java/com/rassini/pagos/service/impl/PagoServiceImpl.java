c2579a4a783948d394c0a717fc022e3ae20b98da
amoralesg-dev <amoralesg@rassini.com>
2026-09-02 21:19:31 -0600

    Revert "Merge pull request #92 from amoralesg-dev/DEV"
    This reverts commit 0945d9d596d1a858746e37e9b6e16a3eeae58bf0, reversing
    changes made to 5379e409a39a50472682ed99f73268c573c9601a.

--- src/main/java/com/rassini/pagos/service/impl/PagoServiceImpl.java
+++ src/main/java/com/rassini/pagos/service/impl/PagoServiceImpl.java
@@ -600,7 +600,7 @@ public class PagoServiceImpl implements PagoService {
                             : "";
 
 
-            if(ConstantsSuppliers.BREAKES.equalsIgnoreCase(pago.getEmpresa()) || ConstantsSuppliers.SUSPENSIONES.equalsIgnoreCase(pago.getEmpresa())){
+            if(ConstantsSuppliers.Breakes.equalsIgnoreCase(pago.getEmpresa())){
                 String ruteo = supplier.getRoutingCodeAba();
 
                 if (ruteo == null || ruteo.isBlank()) {
